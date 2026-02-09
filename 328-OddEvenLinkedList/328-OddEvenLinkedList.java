3ba108f57c9b0e60e5778e2f3a851d6944f1c699
PRIYADHARSHINI M <mpriyadharshinia8@gmail.com>
2026-02-09 16:10:37 +0530

    Create 328-OddEvenLinkedList

---
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.java
@@ -0,0 +1,28 @@
+// Last updated: 2/9/2026, 4:10:39 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode a = head;
+        ListNode b = head.next, c = b;
+        while (b != null && b.next != null) {
+            a.next = b.next;
+            a = a.next;
+            b.next = a.next;
+            b = b.next;
+        }
+        a.next = c;
+        return head;
+    }
+}
