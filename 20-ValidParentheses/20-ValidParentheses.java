fe4fbb3ce433929e6f0bc498915fe36592761011
PRIYADHARSHINI M <mpriyadharshinia8@gmail.com>
2026-02-09 16:11:43 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,18 @@
+// Last updated: 2/9/2026, 4:11:44 PM
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stk = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stk.push(c);
+            } else if (stk.isEmpty() || !match(stk.pop(), c)) {
+                return false;
+            }
+        }
+        return stk.isEmpty();
+    }
+
+    private boolean match(char l, char r) {
+        return (l == '(' && r == ')') || (l == '{' && r == '}') || (l == '[' && r == ']');
+    }
+}
