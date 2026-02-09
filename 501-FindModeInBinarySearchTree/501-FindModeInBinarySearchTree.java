05072132ba6f6181fc64d0b6f3bed56993b5e6f1
PRIYADHARSHINI M <mpriyadharshinia8@gmail.com>
2026-02-09 16:10:32 +0530

    Create 501-FindModeInBinarySearchTree

---
+++ 501-FindModeInBinarySearchTree/501-FindModeInBinarySearchTree.java
@@ -0,0 +1,53 @@
+// Last updated: 2/9/2026, 4:10:34 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private Integer prev = null;
+    private int count = 0;
+    private int maxCount = 0;
+    private List<Integer> modes = new ArrayList<>();
+
+    public int[] findMode(TreeNode root) {
+        inOrder(root);
+        int[] result = new int[modes.size()];
+        for (int i = 0; i < modes.size(); i++) {
+            result[i] = modes.get(i);
+        }
+        return result;
+    }
+    private void inOrder(TreeNode node) {
+        if (node == null) return;
+
+        inOrder(node.left); 
+        if (prev != null && node.val == prev) {
+            count++;
+        } else {
+            count = 1;
+        }
+
+        if (count > maxCount) {
+            modes.clear();            
+            modes.add(node.val);
+            maxCount = count;
+        } else if (count == maxCount) {
+            modes.add(node.val);     
+        }
+
+        prev = node.val;
+
+        inOrder(node.right); 
+    }
+}
