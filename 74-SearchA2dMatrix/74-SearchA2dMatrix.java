a0264938bcc7ab264bb5fce44c65f30a666ba462
PRIYADHARSHINI M <mpriyadharshinia8@gmail.com>
2026-02-09 16:11:27 +0530

    Create 74-SearchA2dMatrix

---
+++ 74-SearchA2dMatrix/74-SearchA2dMatrix.java
@@ -0,0 +1,17 @@
+// Last updated: 2/9/2026, 4:11:29 PM
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int left = 0, right = m * n - 1;
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            int x = mid / n, y = mid % n;
+            if (matrix[x][y] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return matrix[left / n][left % n] == target;
+    }
+}
